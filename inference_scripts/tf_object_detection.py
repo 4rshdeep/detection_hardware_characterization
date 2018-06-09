a66eb48d679868ebaebde3400fd889a4637fd33b
arshdeep singh <arshdeepiitd@gmail.com>
2018-06-09 17:45:07 +0530

    tensorflow benchmarking script modified: added an empty pass

--- inference_scripts/tf_object_detection.py
+++ inference_scripts/tf_object_detection.py
@@ -1,102 +1,38 @@
-
 # coding: utf-8
-
-# # Object Detection Demo
-# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
-
-# # Imports
-
-# In[49]:
-
-
 import numpy as np
 import os
 import six.moves.urllib as urllib
 import sys
+import time 
 import tarfile
 import tensorflow as tf
 import zipfile
-
 from collections import defaultdict
 from io import StringIO
+# # import matplotlib
+# matplotlib.use('TkAgg')
+import matplotlib; matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from PIL import Image
 
-# This is needed since the notebook is stored in the object_detection folder.
-
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
-
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
 CONFIDENCE=0.35
 DPI=96
-# ## Env setup
-
-# In[2]:
-
-
-# This is needed to display the images.
-# get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[50]:
-
-
-from object_detection.utils import label_map_util
-
-from object_detection.utils import visualization_utils as vis_util
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[40]:
-
 
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-# MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
 NUM_CLASSES = 90
 
 
-# ## Download Model
-
-# In[41]:
-
-
-# opener = urllib.request.URLopener()
-# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-# tar_file = tarfile.open(MODEL_FILE)
-# for file in tar_file.getmembers():
-#   file_name = os.path.basename(file.name)
-#   if 'frozen_inference_graph.pb' in file_name:
-#     tar_file.extract(file, os.getcwd())
-
-
 # ## Load a (frozen) Tensorflow model into memory.
-
-# In[42]:
-
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -106,50 +42,25 @@ with detection_graph.as_default():
     tf.import_graph_def(od_graph_def, name='')
 
 
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[43]:
-
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-
 # ## Helper code
-
-# In[44]:
-
-
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
-# # Detection
-
-# In[45]:
-
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
+# PATH_TO_TEST_IMAGES_DIR = 'test_images'
+PATH_TO_TEST_IMAGES_DIR = '../benchmark/images/'
+
 tmpImgs = os.listdir(PATH_TO_TEST_IMAGES_DIR)
-TEST_IMAGE_PATHS = []
+TEST_IMAGES = []
+
 for img in tmpImgs:
-  TEST_IMAGE_PATHS.append(os.path.join(PATH_TO_TEST_IMAGES_DIR, img))
-# TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-print (TEST_IMAGE_PATHS)
-# Size, in inches, of the output images.
-# IMAGE_SIZE = (300/DPI, 300/DPI)
-
-
-# In[46]:
-
+  TEST_IMAGES.append(os.path.join(PATH_TO_TEST_IMAGES_DIR, img))
 
 def run_inference_for_single_image(image, graph):
   with graph.as_default():
@@ -198,39 +109,87 @@ def run_inference_for_single_image(image, graph):
   return output_dict
 
 
-# In[47]:
-for image_path in TEST_IMAGE_PATHS:
-  image = Image.open(image_path)
-  width, height = image.size
-  # the array based representation of the image will be used later in order to prepare the
-  # result image with boxes and labels on it.
-  image_np = load_image_into_numpy_array(image)
-  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-  image_np_expanded = np.expand_dims(image_np, axis=0)
-  # Actual detection.
-  output_dict = run_inference_for_single_image(image_np, detection_graph)
-#   Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks'),
-      use_normalized_coordinates=True,
-      line_thickness=5)
-  
+with detection_graph.as_default():
+  with tf.Session() as sess:
+    ops = tf.get_default_graph().get_operations()
+    all_tensor_names = {output.name for op in ops for output in op.outputs}
+    tensor_dict = {}
+    for key in [
+        'num_detections', 'detection_boxes', 'detection_scores',
+        'detection_classes', 'detection_masks'
+    ]:
+      tensor_name = key + ':0'
+      if tensor_name in all_tensor_names:
+        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
 
-  for i,score in enumerate(output_dict['detection_scores']):
-    if score > CONFIDENCE:
-      org_detection_box = list(output_dict['detection_boxes'][i])
-      output_dict['detection_boxes'][i][0]=org_detection_box[1]*width
-      output_dict['detection_boxes'][i][1]=org_detection_box[0]*height
-      output_dict['detection_boxes'][i][2]=org_detection_box[3]*width
-      output_dict['detection_boxes'][i][3]=org_detection_box[2]*height
-      print (category_index[int(output_dict['detection_classes'][i])]['name']+": "+str(output_dict['detection_boxes'][i])+" : "+str(score))
-  plt.figure()
-  plt.imshow(image_np)
-  plt.show()
-  print ("===========================")
+    ########## First pass empty ####################
+    img = TEST_IMAGES[0]
+    img = Image.open(img)
+    img = load_image_into_numpy_array(img)
+    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+    t1 = time.time()
+    sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(img, 0)})
+    t2 = time.time()
+    fst_inference = (t2-t1)
+
+
+    inf_time  = 0
+    read_time = 0
+    vis_time  = 0
+
+    for image_path in TEST_IMAGES:
+      
+      t1 = time.time()
+      image = Image.open(image_path)
+      width, height = image.size
+      image = load_image_into_numpy_array(image)
+      t2 = time.time()
+      print("[INFO] Time taken in loading image is {}".format(t2-t1))
+      read_time += (t2-t1)
+
+      # Run inference
+      t1 = time.time()
+      output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
+      t2 = time.time()
+      print("[INFO] Time taken in inference is {}".format(t2-t1))
+      inf_time += (t2-t1)
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        print("detection_masks ftw!")
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+
+      t1 = time.time()
+      vis_util.visualize_boxes_and_labels_on_image_array(
+              image,
+              output_dict['detection_boxes'],
+              output_dict['detection_classes'],
+              output_dict['detection_scores'],
+              category_index,
+              instance_masks=output_dict.get('detection_masks'),
+              use_normalized_coordinates=True,
+              line_thickness=5)
+      t2 = time.time()
+      print("[INFO] Time taken in Visualization is {}".format(t2-t1))
+      print ("===========================")
+
+
+num_imgs = len(TEST_IMAGES)
+
+print("\n[INFO] Time taken in First inference is {}".format(fst_inference))
+
+print("\n[INFO] Total Time taken in loading image is {}".format(read_time))    
+print("[INFO] Total Time taken in inference is {}".format(inf_time))
+print("[INFO] Total Time taken in Visualization is {}".format(vis_time))
+
+
+print("\n[INFO] Average Time taken in loading image is {}".format(read_time/num_imgs))    
+print("[INFO] Average Time taken in inference is {}".format(inf_time/num_imgs))
+print("[INFO] Average Time taken in Visualization is {}".format(vis_time/num_imgs))
+
+      
 
